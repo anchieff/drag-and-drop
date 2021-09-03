595e0e80bb1805f3f7d80df3e3f16f8ff3f87e61
anchieff <info@annanekrasova.ru>
2021-09-03 18:19:15 +0400

    feat: drag&drop

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
-// Project Type
+// Drag & Drop Interfaces ==========================================================================
+interface Draggable {
+    dragStartHandler(event: DragEvent): void
+    dragEndHandler(event: DragEvent): void
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void
+    dropHandler(event: DragEvent): void
+    dragLeaveHandler(event: DragEvent): void
+}
+
+// Project Type ====================================================================================
 enum ProjectStatus { Active, Finished }
 
 class Project {
@@ -10,7 +22,7 @@ class Project {
         public status: ProjectStatus) {}
 }
 
-// Project State Managment
+// Project State Managment =========================================================================
 type Listener<T> = (items: T[]) => void
 
 class State<T> {
@@ -46,8 +58,19 @@ class ProjectState extends State<Project>{
             numOfPeople, 
             ProjectStatus.Active
         ) 
-
         this.projects.push(newProject)
+        this.updateListeners() 
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId)
+        if (project && project.status !== newStatus) {
+            project.status = newStatus
+            this.updateListeners()
+        }
+    }
+
+    private updateListeners() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice())
         }
@@ -56,7 +79,7 @@ class ProjectState extends State<Project>{
 
 const projectState = ProjectState.getInstance()
 
-// Validation
+// Validation ======================================================================================
 interface Validatable {
     value: string | number
     required?: boolean
@@ -92,7 +115,7 @@ function validate(validatableInput: Validatable) {
     return isValid
 }
 
-// autobind decorator
+// autobind decorator ===========================================================================
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value
     const adjDescriptor: PropertyDescriptor = {
@@ -141,8 +164,50 @@ abstract class Component <T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void
 }
 
+// Project Item Class ==========================================================================
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+    private project: Project
+
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person'
+        } else {
+            return `${this.project.people} persons`
+        }
+    }
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id)
+        this.project = project
+
+        this.configure()
+        this.renderContent()
+    }
+
+    @autobind
+    dragStartHandler(event: DragEvent) {
+        event.dataTransfer!.setData('text/plain', this.project.id)
+        event.dataTransfer!.effectAllowed = 'move'
+    }
+
+    dragEndHandler(_: DragEvent) {
+        console.log('DRAG END')
+    }
+
+    configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler)
+        this.element.addEventListener('dragend', this.dragEndHandler)
+    }
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
+        this.element.querySelector('p')!.textContent = this.project.description
+    }
+}
+
 // ProjectList Class ===========================================================================
-class ProjectList extends Component<HTMLDivElement, HTMLElement>{
+class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
     assignedProjects: Project[]
 
     constructor(private type: 'active' | 'finished') {
@@ -153,7 +218,32 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         this.renderContent()
     }
 
+    @autobind
+    dragOverHandler(event: DragEvent) {
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault()
+            const listEl = this.element.querySelector('ul')!
+            listEl.classList.add('droppable')
+        }
+    }
+
+    @autobind
+    dropHandler(event: DragEvent) {
+        const prjId = event.dataTransfer!.getData('text/plain')
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
+    }
+
+    @autobind
+    dragLeaveHandler(_: DragEvent) {
+        const listEl = this.element.querySelector('ul')!
+        listEl.classList.remove('droppable')
+    }
+
     configure() {
+        this.element.addEventListener('dragover', this.dragOverHandler)
+        this.element.addEventListener('dragleave', this.dragLeaveHandler)
+        this.element.addEventListener('drop', this.dropHandler)
+
         projectState.addListener((projects: Project[]) => {
             const relevantProjects = projects.filter(prj => {
                 if (this.type === 'active') {
@@ -176,16 +266,14 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement
         listEl.innerHTML = ''
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li')
-            listItem.textContent = prjItem.title
-            listEl.appendChild(listItem)
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
         }
 
     }
 }
 
 
-// ProjectInput Class
+// ProjectInput Class ==========================================================================
 class ProjectInput extends Component <HTMLDivElement, HTMLFormElement>{
     titleInputElement: HTMLInputElement
     descriptionInputElement: HTMLInputElement
